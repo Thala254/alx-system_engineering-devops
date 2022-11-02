e53da0c323a60d80b361cc8ed8702e90242bdde4
thala254 <thalamine@gmail.com>
2022-11-03 02:51:28 +0300

    added 0x18-webstack_monitoring project with task files and a readme

---
+++ 0x18-webstack_monitoring/get_dashboard.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Retrives a list of dashboards with details.
+    Run in bash:
+     DD_SITE="us5.datadoghq.com" DD_API_KEY="xxx" DD_APP_KEY="xxx" python3 get_dashboard.py
+"""
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.dashboards_api import DashboardsApi
+
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = DashboardsApi(api_client)
+    response = api_instance.list_dashboards(
+        filter_shared=False
+    )
+
+    print(response)
---
+++ 0x18-webstack_monitoring/validate_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Validates visibility of my web server in Datadog.
+    Get all hosts for your organization returns "OK" response
+    Run in bash:
+     DD_SITE="us5.datadoghq.com" DD_API_KEY="xxx" DD_APP_KEY="xxx" python3 validate_server.py
+"""
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.hosts_api import HostsApi
+
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = HostsApi(api_client)
+    response = api_instance.list_hosts(
+        filter="env:ci",
+    )
+
+    print(response)
